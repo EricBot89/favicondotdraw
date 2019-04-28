1bd63154450f0461b68b9b82340ff753e80bf877
EricBot89 <eloucks@gmail.com>
2019-04-28 14:55:05 -0400

    fixed an issue with port env variable

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = require("./index");
 const models = require("./models");
 const server = http.createServer(app);
 
-const port = 8080;
+const PORT = 8080;
 
 const init = async () => {
   await models.Favicon.sync();
   server.listen(port, () => {
-    console.log(`I am a server, hi. I can hear things on port ${port}`);
+    console.log(`I am a server, hi. I can hear things on port ${PORT}`);
   });
 }
 
